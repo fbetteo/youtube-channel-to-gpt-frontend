ff73807c473cb453cdc09aae653c2b93f6a9f2f7
fbetteo <francobetteo@gmail.com>
2025-04-04 18:45:55 -0300

    comment unused

--- app/utils/checkSession.tsx
+++ app/utils/checkSession.tsx
@@ -1,43 +1,43 @@
-import { useEffect, useState } from 'react';
-import { createBrowserClient } from '@supabase/ssr';
-import { useGlobalStore } from '../store/store';
-import axios from 'axios';
-import { UserData, sessionData } from '../types/types';
-import { v4 as uuidv4 } from 'uuid';
+// import { useEffect, useState } from 'react';
+// import { createBrowserClient } from '@supabase/ssr';
+// import { useGlobalStore } from '../store/store';
+// import axios from 'axios';
+// import { UserData, sessionData } from '../types/types';
+// import { v4 as uuidv4 } from 'uuid';
 
-async function checkSession(): Promise<sessionData | undefined> {
-    const { modifyjwtToken, modifySubscription } = useGlobalStore.getState();
-    const supabase = createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
-    const maxRetries = 3;
-    try {
-        const sessionResult = await supabase.auth.getSession();
-        const jwt_token = sessionResult?.data?.session?.access_token;
+// async function checkSession(): Promise<sessionData | undefined> {
+//     const { modifyjwtToken, modifySubscription } = useGlobalStore.getState();
+//     const supabase = createBrowserClient(
+//         process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//     )
+//     const maxRetries = 3;
+//     try {
+//         const sessionResult = await supabase.auth.getSession();
+//         const jwt_token = sessionResult?.data?.session?.access_token;
 
-        if (!jwt_token) {
-            console.error("JWT token is not available")
-            return undefined;
-        }
+//         if (!jwt_token) {
+//             console.error("JWT token is not available")
+//             return undefined;
+//         }
 
-        const resource = await modifyjwtToken(jwt_token);
-        try {
-            const subscription = await axios.get<UserData>(process.env.NEXT_PUBLIC_API_URL + '/get_user_data', {
-                headers: { "Authorization": `Bearer ${jwt_token}` }
-            })
-            modifySubscription(subscription.data.subscription)
-            return { "jwtToken": resource, "userData": subscription.data };
+//         const resource = await modifyjwtToken(jwt_token);
+//         try {
+//             const subscription = await axios.get<UserData>(process.env.NEXT_PUBLIC_API_URL + '/get_user_data', {
+//                 headers: { "Authorization": `Bearer ${jwt_token}` }
+//             })
+//             modifySubscription(subscription.data.subscription)
+//             return { "jwtToken": resource, "userData": subscription.data };
 
-        } catch (error) {
-            console.error('User Just Signed up', error);
-            const subscription = { data: { uuid: uuidv4(), email: "", subscription: "free", count_messages: 0 } }
-            return { "jwtToken": resource, "userData": subscription.data };
-        }
+//         } catch (error) {
+//             console.error('User Just Signed up', error);
+//             const subscription = { data: { uuid: uuidv4(), email: "", subscription: "free", count_messages: 0 } }
+//             return { "jwtToken": resource, "userData": subscription.data };
+//         }
 
-    } catch (error) {
-        console.error('Error getting the token', error);
-    }
-}
+//     } catch (error) {
+//         console.error('Error getting the token', error);
+//     }
+// }
 
-export default checkSession;
+// export default checkSession;
--- app/utils/useCheckSession.ts
+++ app/utils/useCheckSession.ts
@@ -1,68 +1,68 @@
-import { useEffect } from 'react';
-import { createBrowserClient } from '@supabase/ssr';
-import { useGlobalStore } from '../store/store';
-import axios from 'axios';
-import { UserData } from '../types/types';
+// import { useEffect } from 'react';
+// import { createBrowserClient } from '@supabase/ssr';
+// import { useGlobalStore } from '../store/store';
+// import axios from 'axios';
+// import { UserData } from '../types/types';
 
 
-const useCheckSession = () => {
-    const supabase = createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
-    const {jwtToken, modifyjwtToken, modifySubscription, modifyEmail} = useGlobalStore();
+// const useCheckSession = () => {
+//     const supabase = createBrowserClient(
+//         process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//     )
+//     const {jwtToken, modifyjwtToken, modifySubscription, modifyEmail} = useGlobalStore();
 
-    useEffect(() => {
-        supabase.auth.getSession()
-            .then(async (session) => {
-                const jwt_token = session?.data?.session?.access_token;
-                // console.log(jwt_token + "pepepep USE EFFECT");
-                if (!jwt_token) {
-                    console.error("JWT token is not available.");
-                    return;
-                }
-                modifyjwtToken(jwt_token);
+//     useEffect(() => {
+//         supabase.auth.getSession()
+//             .then(async (session) => {
+//                 const jwt_token = session?.data?.session?.access_token;
+//                 // console.log(jwt_token + "pepepep USE EFFECT");
+//                 if (!jwt_token) {
+//                     console.error("JWT token is not available.");
+//                     return;
+//                 }
+//                 modifyjwtToken(jwt_token);
 
-                try {
+//                 try {
 
-                    const subscription = await axios.get<UserData>(process.env.NEXT_PUBLIC_API_URL + '/get_user_data', {
-                        headers: { "Authorization": `Bearer ${jwt_token}` }
-                        // Include additional data as needed
-                    })
-                    // console.log(subscription.data)
-                    // console.log("USE EFFECT CHECK SESSion")
-                    modifySubscription(subscription.data.subscription)
-                    modifyEmail(subscription.data.email)
+//                     const subscription = await axios.get<UserData>(process.env.NEXT_PUBLIC_API_URL + '/get_user_data', {
+//                         headers: { "Authorization": `Bearer ${jwt_token}` }
+//                         // Include additional data as needed
+//                     })
+//                     // console.log(subscription.data)
+//                     // console.log("USE EFFECT CHECK SESSion")
+//                     modifySubscription(subscription.data.subscription)
+//                     modifyEmail(subscription.data.email)
 
-                }
-                catch (error) {
-                    console.error('Error getting the token', error);
-                    const base64Url = jwt_token.split('.')[1]; // Get the payload part
-                    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-                    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-                        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-                    }).join(''));
-                    // const subscription = await axios.get<UserData>(process.env.NEXT_PUBLIC_API_URL + '/get_user_data', {
-                    //     headers: { "Authorization": `Bearer ${jwt_token}` }
-                    // })
-                    const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/users', {
+//                 }
+//                 catch (error) {
+//                     console.error('Error getting the token', error);
+//                     const base64Url = jwt_token.split('.')[1]; // Get the payload part
+//                     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//                     const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+//                         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+//                     }).join(''));
+//                     // const subscription = await axios.get<UserData>(process.env.NEXT_PUBLIC_API_URL + '/get_user_data', {
+//                     //     headers: { "Authorization": `Bearer ${jwt_token}` }
+//                     // })
+//                     const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/users', {
 
-                        email: JSON.parse(jsonPayload).email,
-                        subscription: 'free'
-                    }
-                        // I'm using sessioncheck because I had issues with jwtToken_zustand being undefined because it took longer to update the global store. I think now it works both ways but I'm not sure.
-                        , { headers: { "Authorization": `Bearer ${jwtToken}` } }
-                    );
-                    modifySubscription('free')
-                    modifyEmail(JSON.parse(jsonPayload).email)
+//                         email: JSON.parse(jsonPayload).email,
+//                         subscription: 'free'
+//                     }
+//                         // I'm using sessioncheck because I had issues with jwtToken_zustand being undefined because it took longer to update the global store. I think now it works both ways but I'm not sure.
+//                         , { headers: { "Authorization": `Bearer ${jwtToken}` } }
+//                     );
+//                     modifySubscription('free')
+//                     modifyEmail(JSON.parse(jsonPayload).email)
 
-                }
+//                 }
             
-                }
-            )
-                .catch(error => console.error('Error getting the token', error));
-        // Assuming you have a function `getJwtToken` that synchronously retrieves the JWT token
-    }, [jwtToken, modifyjwtToken, modifySubscription, modifyEmail ]);
-};
+//                 }
+//             )
+//                 .catch(error => console.error('Error getting the token', error));
+//         // Assuming you have a function `getJwtToken` that synchronously retrieves the JWT token
+//     }, [jwtToken, modifyjwtToken, modifySubscription, modifyEmail ]);
+// };
 
-export default useCheckSession;
+// export default useCheckSession;
